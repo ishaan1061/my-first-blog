6197fba8fafe062c823c442f5e9e0aeba54a0c62
Your Name <you@example.com>
2018-12-10 00:19:48 +0530

    added all the required templates

--- blog/models.py
+++ blog/models.py
@@ -8,8 +8,10 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
     published_date = models.DateTimeField(blank=True, null=True)
+    created_date=""
     def publish(self):
         self.published_date = timezone.now()
+        self.created_date = timezone.now()
         self.save()
 
     def __str__(self):
